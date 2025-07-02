df65389629edb41b6e0935ca50bca23a647c5371
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2025-07-02 11:27:42 +0530

    ADD: Initial setup code

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='DSA-Package',
+    version='0.1',
+    packages=find_packages(),  # auto-discovers __init__.py folders
+    author='Sourabh',
+    description='A toolkit for sorting and hashing algorithms',
+)
